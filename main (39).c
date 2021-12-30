3dc05728a86c79f05d06f5a94acebb1088c851b7
KAUSHAL CHAUHAN <93530886+kaushalch5287@users.noreply.github.com>
2021-12-30 07:43:05 +0530

    Update main (39).c

--- main (39).c
+++ main (39).c
@@ -6,11 +6,7 @@ C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
 Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
-//5 5 5 5 5
-//4 4 4 4 
-//3 3 3 
-//2 2 
-//1 
+
 #include <stdio.h>
 
 int main()
